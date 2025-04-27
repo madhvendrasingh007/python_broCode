7e91748c43bd326c2ce23e22b6a824e2e0ea2f31
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-04-27 18:30:05 +0000

    while loop

---
+++ 01_basics/12_whileLoop.py
@@ -0,0 +1,10 @@
+# while loop -> it executes a block of code as long as a specified condition is true
+
+
+food = input("Enter a food (q to quit): ")
+
+while not food == "q":
+    print(f"You like: {food}")
+    food = input("Enter another food (q to quit): ")
+
+print("bye!")
