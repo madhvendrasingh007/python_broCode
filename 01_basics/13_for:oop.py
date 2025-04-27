91aa56373280920107cde005a49eec6d52ca796e
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-04-27 19:06:18 +0000

    for loop

---
+++ 01_basics/13_for:oop.py
@@ -0,0 +1,4 @@
+# for loop -> it iterates over a sequence (like a list, tuple, or string) and executes a block of code for each item in the sequence
+
+for x in reversed(range(1,11)): 
+    print(x)
