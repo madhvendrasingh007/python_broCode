726085fa1ef8abcb95abdacde8d3f273df12caf4
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-04-27 19:52:11 +0000

    updated file

--- 01_basics/14_countdown.py
+++ 01_basics/14_countdown.py
@@ -6,7 +6,7 @@ for x in range(myTime, 0, -1):
     Min = int(x/60)%60
     Hour = int(x/3600)%24
     print(f"{Hour:02}:{Min:02}:{Sec:02}")
-    time.sleep(1)
+    time.sleep(1) 
 print("Time's up!")
 # This program takes a number of seconds as input and counts down to zero, printing each second as it goes.
 # It uses the time.sleep() function to pause for one second between each print statement.
