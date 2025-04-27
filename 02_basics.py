89ca1a2e945b0e9c4208715813cb1eabdc8c5ea8
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-04-27 08:25:42 +0000

    variables added

--- 02_basics.py
+++
@@ -1,7 +0,0 @@
-# Variable = Sring, int, float, bool
-
-first_name = "John" 
-last_name = "Doe"
-age = 30
-
-print(f"Hello, my name is {first_name} {last_name} and I am {age} years old.")
---
+++ 02_variables.py
@@ -0,0 +1,16 @@
+# Variable = Sring, int, float, bool
+
+first_name = "Kevin"
+last_name = "Robinson"
+age = 30
+height = 5.9
+is_student = True
+
+print(f"Hello, my name is {first_name} {last_name} and I am {age} years old.")
+print(f"My height is {height} feet and it is {is_student} that I am a student.")
+
+print(type(first_name)) # str
+print(type(last_name))  # str
+print(type(age))        # int
+print(type(height)) # float 
+print(type(is_student)) # bool
